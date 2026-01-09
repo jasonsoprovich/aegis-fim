a7776e5f159b69a8f49324cd753436a36c1e615e
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2026-01-08 19:20:22 -0700

    feat: add calc_sha256 function and swap ph

--- hash_engine.py
+++ hash_engine.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 # def list_files_recursively(path="."):
@@ -9,12 +10,23 @@ import os
 #             print(full_path)
 
 
+def calc_sha256(filepath):
+    sha256_hash = hashlib.sha256()
+    try:
+        with open(filepath, "rb") as f:
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
+    except (PermissionError, FileNotFoundError):
+        return None
+
+
 def set_baseline(directory):
     baseline = {}
     for root, _, files in os.walk(directory):
         for file in files:
             abs_path = os.path.abspath(os.path.join(root, file))
-            file_hash = "placeholder function"
+            file_hash = calc_sha256(abs_path)
             baseline[abs_path] = file_hash
 
     return baseline
